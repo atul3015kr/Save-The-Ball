0f26382172b07727324069b6d08e23184d201d12
Atulya Kumar Pandey <atul3015@gmail.com>
2017-07-08 20:56:06 +0530

    Update Racquet1.java

--- src/p1/Racquet1.java
+++ src/p1/Racquet1.java
@@ -29,9 +29,9 @@ public class Racquet1 {
 	}
 
 	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_UP)
+		if (e.getKeyCode() == KeyEvent.VK_W)
 			ya = -Game.speed;
-		if (e.getKeyCode() == KeyEvent.VK_DOWN)
+		if (e.getKeyCode() == KeyEvent.VK_S)
 			ya = Game.speed;
 	}
 
